379aa40f6c529cc3dd7427ca7dac30c992486b6d
akeboshi <akeboshi82@gmail.com>
2022-11-10 18:38:41 +0800

    fix wechat phaser 加载本地 音频视频 问题

--- js/scene/UIScene.js
+++ js/scene/UIScene.js
@@ -4,54 +4,12 @@ export class UIScene extends Phaser.Scene {
     constructor() {
         super();
         this.key = UISCENE;
-        this.init();
+        this.initWX();
         // console.log("uiscene init");
     }
-    init(data) {
-        this.dragonbone = data.display;
-    }
-
-    preload() {
-        this.load.image("btn", "images/bullet.png");
-    }
-    create() {
-        // console.log("uiscene create");
-        // const con = this.make.container();
-        // this.img = this.make.image({ key: "btn" });
-        // con.setPosition(100,100);
-        // con.add(this.img);
-        // con.setSize(this.img.width, this.img.height);
-        // // con.setScale(2);
-        // con.setInteractive();
-        // con.on("pointerdown", this.pointerDownHandler, this);
 
 
-
-        this.mBackground = this.add.graphics({ x: 100, y: 50 });
-        this.mBackground.fillStyle(0x0000, 1);
-        this.mBackground.fillRect(0, 0, 100, 100).setInteractive(new Phaser.Geom.Rectangle(0, 0, 100, 100), Phaser.Geom.Rectangle.Contains);
-        this.mBackground.on("pointerdown", this.pointerDownHandler, this);
-
-        this.mAttackBtn = this.add.graphics({ x: 200, y: 50 });
-        this.mAttackBtn.fillStyle(0xffcc00, 1);
-        this.mAttackBtn.fillRect(0, 0, 100, 100).setInteractive(new Phaser.Geom.Rectangle(0, 0, 100, 100), Phaser.Geom.Rectangle.Contains);
-        this.mAttackBtn.on("pointerdown", this.pointerAttackHandler, this);
-
-
-        this.mShareBtn = this.add.graphics({ x: 300, y: 50 });
-        this.mShareBtn.fillStyle(0xffffff, 1);
-        this.mShareBtn.fillRect(0, 0, 100, 100).setInteractive(new Phaser.Geom.Rectangle(0, 0, 100, 100), Phaser.Geom.Rectangle.Contains);
-        this.mShareBtn.on("pointerdown", this.pointerShareHandler, this);
-    }
-
-    update() {
-
-    }
-
-
-
-
-    init() {
+    initWX() {
         wx.showShareMenu({
             withShareTicket: true,
             menus: ['shareAppMessage', 'shareTimeline']
@@ -68,6 +26,64 @@ export class UIScene extends Phaser.Scene {
         });
         console.log("share game --->");
     }
+    init(data) {
+        this.dragonbone = data.display;
+    }
+
+    preload() {
+        this.load.image("btn", "images/bullet.png");
+        this.load.audio('bgSound', "images/mp3/explore1.mp3");//'https://akeboshi1.github.io/Jay/publish/assets/Jay%20demo%201.mp3');
+    }
+    create() {
+        // console.log("uiscene create");
+        // const con = this.make.container();
+        // this.img = this.make.image({ key: "btn" });
+        // con.setPosition(100,100);
+        // con.add(this.img);
+        // con.setSize(this.img.width, this.img.height);
+        // // con.setScale(2);
+        // con.setInteractive();
+        // con.on("pointerdown", this.pointerDownHandler, this);
+
+
+        this.sfx = this.sound.add('bgSound');
+        this.sfx.play();
+        this.mBackground = this.add.graphics({ x: 100, y: 50 });
+        this.mBackground.fillStyle(0x0000, 1);
+        this.mBackground.fillRect(0, 0, 100, 100).setInteractive(new Phaser.Geom.Rectangle(0, 0, 100, 100), Phaser.Geom.Rectangle.Contains);
+        this.mBackground.on("pointerdown", this.pointerDownHandler, this);
+
+        this.mAttackBtn = this.add.graphics({ x: 200, y: 50 });
+        this.mAttackBtn.fillStyle(0xffcc00, 1);
+        this.mAttackBtn.fillRect(0, 0, 100, 100).setInteractive(new Phaser.Geom.Rectangle(0, 0, 100, 100), Phaser.Geom.Rectangle.Contains);
+        this.mAttackBtn.on("pointerdown", this.pointerAttackHandler, this);
+
+
+        this.mShareBtn = this.add.graphics({ x: 300, y: 50 });
+        this.mShareBtn.fillStyle(0xffffff, 1);
+        this.mShareBtn.fillRect(0, 0, 100, 100).setInteractive(new Phaser.Geom.Rectangle(0, 0, 100, 100), Phaser.Geom.Rectangle.Contains);
+        this.mShareBtn.on("pointerdown", this.pointerShareHandler, this);
+
+        this.mMusicBtn = this.add.graphics({ x: 400, y: 50 });
+        this.mMusicBtn.fillStyle(0x00ff00, 1);
+        this.mMusicBtn.fillRect(0, 0, 100, 100).setInteractive(new Phaser.Geom.Rectangle(0, 0, 100, 100), Phaser.Geom.Rectangle.Contains);
+        this.mMusicBtn.on("pointerdown", this.pointerMusicHandler, this);
+    }
+
+    update() {
+
+    }
+
+
+    pointerMusicHandler() {
+        if (!this.pause) {
+            this.sfx.pause();
+        } else {
+            this.sfx.play();
+        }
+        this.pause = !this.pause;
+    }
+
 
     pointerShareHandler() {
         wx.shareAppMessage({
